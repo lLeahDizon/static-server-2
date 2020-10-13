f340ef1c624901806e880da5a064600af4482343
Lemon <516330797@qq.com>
2020-10-13 17:33:03 +0800

    完成登陆注册代码

--- server.js
+++ server.js
@@ -23,34 +23,58 @@ var server = http.createServer(function (request, response) {
 
   console.log("有个傻子发请求过来啦！路径（带查询参数）为：" + pathWithQuery);
 
-  response.statusCode = 200;
-  // 默认首页
-  const filePath = path === "/" ? "/index.html" : path;
-  const index = filePath.lastIndexOf(".");
-  // suffix 后缀
-  const suffix = filePath.substring(index);
-  const fileTypes = {
-    ".html": "text/html",
-    ".css": "text/css",
-    ".js": "text/javascript",
-    ".png": "image/png",
-    ".jpg": "image/jpeg",
-  };
-  response.setHeader(
-    "Content-Type",
-    `${fileTypes[suffix] || "text/html"};charset=utf-8`
-  );
-  let content;
-  try {
-    content = fs.readFileSync(`public${filePath}`);
-  } catch (error) {
-    content = "文件不存在";
-    response.statusCode = 404;
+  if (path === "/register" && method === "POST") {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    const userArray = JSON.parse(fs.readFileSync("./db/users.json"));
+    const array = [];
+    request.on("data", (chunk) => {
+      array.push(chunk);
+    });
+    request.on("end", () => {
+      const string = Buffer.concat(array).toString();
+      const obj = JSON.parse(string);
+      console.log(obj.name);
+      console.log(obj.password);
+      const lastUser = userArray[userArray.length - 1];
+      // id 为最后一个用户的 id + 1
+      const newUser = {
+        id: lastUser ? lastUser.id + 1 : 1,
+        name: obj.name,
+        password: obj.password,
+      };
+      userArray.push(newUser);
+      fs.writeFileSync("./db/users.json", JSON.stringify(userArray));
+      response.end("很好");
+    });
+  } else {
+    response.statusCode = 200;
+    // 默认首页
+    const filePath = path === "/" ? "/index.html" : path;
+    const index = filePath.lastIndexOf(".");
+    // suffix 后缀
+    const suffix = filePath.substring(index);
+    const fileTypes = {
+      ".html": "text/html",
+      ".css": "text/css",
+      ".js": "text/javascript",
+      ".png": "image/png",
+      ".jpg": "image/jpeg",
+    };
+    response.setHeader(
+      "Content-Type",
+      `${fileTypes[suffix] || "text/html"};charset=utf-8`
+    );
+    let content;
+    try {
+      content = fs.readFileSync(`public${filePath}`);
+    } catch (error) {
+      content = "文件不存在";
+      response.statusCode = 404;
+    }
+
+    response.write(content);
+    response.end();
   }
-
-  response.write(content);
-  response.end();
-
   /******** 代码结束，下面不要看 ************/
 });
 
